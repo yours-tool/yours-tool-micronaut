1fe8ed9ea44d3cab7b3a8049c2307840ef01b403
wbh <136207024@qq.com>
2023-01-29 15:58:35 +0800

    init

---
+++ src/main/java/yours/tool/micronaut/Application.java
@@ -0,0 +1,10 @@
+package yours.tool.micronaut;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
